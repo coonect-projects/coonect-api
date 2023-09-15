0032f633dfb0a1fc55dd1d394199d12cff42df5d
dukcode <duk9741@gmail.com>
2023-09-15 17:25:18 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/me/coonect/coonect/CoonectApplication.java
@@ -0,0 +1,13 @@
+package me.coonect.coonect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoonectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoonectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/coonect/coonect/CoonectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.coonect.coonect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoonectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
