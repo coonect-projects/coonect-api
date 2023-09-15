3ec7efe88ed13bd3b636f2816ba0629f75dc15e1
dukcode <duk9741@gmail.com>
2023-09-16 07:32:47 +0900

    test: 닉네임 중복 검증 기능 테스트 작성

--- src/test/java/me/coonect/coonect/mock/TestContainer.java
+++ src/test/java/me/coonect/coonect/mock/TestContainer.java
@@ -2,8 +2,11 @@ package me.coonect.coonect.mock;
 
 import lombok.Builder;
 import me.coonect.coonect.member.adapter.in.MemberSignupController;
+import me.coonect.coonect.member.adapter.in.NicknameValidationController;
 import me.coonect.coonect.member.application.domain.service.MemberSignupService;
+import me.coonect.coonect.member.application.domain.service.NicknameValidationService;
 import me.coonect.coonect.member.application.port.in.MemberSignupUseCase;
+import me.coonect.coonect.member.application.port.in.NicknameValidationUseCase;
 import me.coonect.coonect.member.application.port.out.MemberRepository;
 
 public class TestContainer {
@@ -11,15 +14,19 @@ public class TestContainer {
   public MemberRepository memberRepository;
 
   public MemberSignupUseCase memberSignupUseCase;
+  public NicknameValidationUseCase nicknameValidationUseCase;
 
   public MemberSignupController memberSignupController;
+  public NicknameValidationController nicknameValidationController;
 
   @Builder
   public TestContainer() {
     memberRepository = new FakeMemberRepository();
 
     memberSignupUseCase = new MemberSignupService(memberRepository);
+    nicknameValidationUseCase = new NicknameValidationService(memberRepository);
 
     memberSignupController = new MemberSignupController(memberSignupUseCase);
+    nicknameValidationController = new NicknameValidationController(nicknameValidationUseCase);
   }
 }
