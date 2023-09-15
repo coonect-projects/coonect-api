066649aa374bc58db6549d9a0fc442a3936b2f94
dukcode <duk9741@gmail.com>
2023-09-16 05:39:37 +0900

    test: MemberSignupCommand 테스트 추가

---
+++ src/test/java/me/coonect/coonect/common/error/ErrorResponseTest.java
@@ -0,0 +1,40 @@
+package me.coonect.coonect.common.error;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ErrorResponseTest {
+
+  @Test
+  public void errorCode_로_ErrorResponse_를_생성할_수_있다() throws Exception {
+    // given
+    ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+
+    // when
+    ErrorResponse errorResponse = ErrorResponse.of(errorCode);
+
+    // then
+    assertThat(errorResponse.getCode()).isEqualTo(ErrorCode.INTERNAL_SERVER_ERROR.getCode());
+    assertThat(errorResponse.getMessage()).isEqualTo(ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+    assertThat(errorResponse.getReasons()).isEmpty();
+  }
+
+  @Test
+  public void errorCode_와_reason_으로_ErrorResponse_를_생성할_수_있다() throws Exception {
+    // given
+    ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+
+    // when
+    ErrorResponse errorResponse = ErrorResponse.of(errorCode, "원인1", "원인2");
+
+    // then
+    assertThat(errorResponse.getCode()).isEqualTo(ErrorCode.INTERNAL_SERVER_ERROR.getCode());
+    assertThat(errorResponse.getMessage()).isEqualTo(ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+    assertThat(errorResponse.getReasons()).hasSize(2);
+    assertThat(errorResponse.getReasons()).containsExactly("원인1", "원인2");
+  }
+}
