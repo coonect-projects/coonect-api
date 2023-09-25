bdeb1ba9dc251c77007a7d8834587cbaf2c9c025
dukcode <duk9741@gmail.com>
2023-09-25 21:35:31 +0900

    fix: 이메일, 중복 에러 시에 에러코드 다른 문제 해결

--- src/main/java/me/coonect/coonect/member/application/domain/exception/NicknameDuplicationException.java
+++ src/main/java/me/coonect/coonect/member/application/domain/exception/NicknameDuplicationException.java
@@ -1,10 +1,13 @@
 package me.coonect.coonect.member.application.domain.exception;
 
 import me.coonect.coonect.common.error.exception.DuplicationException;
+import me.coonect.coonect.common.error.exception.ErrorCode;
 
 public class NicknameDuplicationException extends DuplicationException {
 
-  public NicknameDuplicationException(final String email) {
-    super(email);
+  private static final ErrorCode errorCode = ErrorCode.NICKNAME_DUPLICATE;
+
+  public NicknameDuplicationException(final String nickname) {
+    super(nickname, errorCode);
   }
 }
