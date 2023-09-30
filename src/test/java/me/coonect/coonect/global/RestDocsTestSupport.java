8ac061301c8bed9c3a45c77e67aa20228482193b
dukcode <duk9741@gmail.com>
2023-09-30 14:18:38 +0900

    test: 로그인 API 테스트 추가
    - RestDocsTestSupport Spring Security 필터 적용

--- src/test/java/me/coonect/coonect/global/RestDocsTestSupport.java
+++ src/test/java/me/coonect/coonect/global/RestDocsTestSupport.java
@@ -3,6 +3,7 @@ package me.coonect.coonect.global;
 import static org.springframework.restdocs.snippet.Attributes.Attribute;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
 import me.coonect.coonect.common.jwt.application.service.JwtProperties;
 import me.coonect.coonect.common.jwt.application.service.JwtService;
 import me.coonect.coonect.common.security.configuration.SecurityConfiguration;
@@ -16,14 +17,17 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.security.config.BeanIds;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 @Disabled
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -48,8 +52,14 @@ public class RestDocsTestSupport {
 
   @BeforeEach
   void setUp(final WebApplicationContext context,
-      final RestDocumentationContextProvider provider) {
+      final RestDocumentationContextProvider provider) throws ServletException {
+
+    DelegatingFilterProxy delegateProxyFilter = new DelegatingFilterProxy();
+    delegateProxyFilter.init(
+        new MockFilterConfig(context.getServletContext(), BeanIds.SPRING_SECURITY_FILTER_CHAIN));
+
     this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+        .addFilter(delegateProxyFilter)
         .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
         .alwaysDo(MockMvcResultHandlers.print())
         .alwaysDo(restDocs)
